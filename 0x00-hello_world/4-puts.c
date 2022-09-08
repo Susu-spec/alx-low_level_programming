b40fe589f5d4e2661eac2d83cfb0aa8028ad7302
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-08 05:31:06 -0700

    Add: Print without printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle")
+#include <stdio.h>
+\**
+*main function
+*
+*Return 0 before exiting function
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
