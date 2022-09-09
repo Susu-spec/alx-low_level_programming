3e1949a3046474f44313791d3c5814a18733dfd0
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-08 20:25:22 -0700

    Add: Print with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+\**
+*main -  entry point
+*
+*printf - print string
+*
+*Return: 0
+*\
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	Return(0);
+}
