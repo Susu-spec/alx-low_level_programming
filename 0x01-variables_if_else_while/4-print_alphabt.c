ac648c13e8c6d7dc424eea2d7933f073b8a1573e
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-09 12:55:51 -0700

    Add: print alphabet without qor e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 indicates success
+ */
+int main(void)
+{
+	int al = 'a';
+
+	while (al <= 'z')
+	{
+		if (al == 'q' || al == 'e')
+		return;
+		putchar(al);
+		al++;
+	}
+	return (0);
+}
