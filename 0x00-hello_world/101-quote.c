ff037f21cde2a1e1e2dff53ead5b9f5ebf857b7d
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-08 21:53:28 -0700

    Modify

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,4 +13,4 @@ int main(void)
 	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19 ");
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", s);
 	return (1);
-}`
+}
