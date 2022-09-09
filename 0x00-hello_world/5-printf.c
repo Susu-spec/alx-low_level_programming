234bab1791e16e76863f2de99c3f915ecff904f6
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-08 20:28:56 -0700

    Modify: 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-\**
+/**
 *main -  entry point
 *
 *printf - print string
 *
-*Return: 0
-*\
+*Return: 0 before exiting function
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
