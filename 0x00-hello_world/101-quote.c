d40a840f95138cbd1f2e2940e9dc77d97ee2ec85
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-08 21:18:59 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * putchar - prints string
+ *
+ * Return: 1 indicates error
+ *
+ */
+int main(void)
+{
+	putchar("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
