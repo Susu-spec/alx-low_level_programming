e45aef96e84fa911f394912a1843f1064c269781
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-12 13:19:24 -0700

    Modify

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,15 +12,14 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = i + 1; j < 10; j++)
 		{
-			if (j != i)
-			{
-				putchar(i + '0');
-				putchar(',');
-				putchar(' ');
-				putchar(j + '0');
-			}
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i == 8 && j == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
