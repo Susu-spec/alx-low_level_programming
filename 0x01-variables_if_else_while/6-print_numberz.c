106fdd6722d62148a39e1c661e9d9f32f6a9e475
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-11 22:35:06 -0700

    Add: print numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 indicates succes
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
