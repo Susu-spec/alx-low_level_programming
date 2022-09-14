23258dc7c83f14dd27d03f33266404042608a2e5
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-14 08:25:43 -0700

    Add: absolute value function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - print absolute value
+ *
+ * @n: int argument
+ *
+ * Return:  absolute value
+ */
+int _abs(int n)
+{
+	if (c < 0 || c == 0)
+		return (c);
+	else
+		return (c * -1);
+}
