5efffdfdd4b6ff2f4ac182330624383b7ea8a583
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-14 13:09:46 -0700

    Print 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - print 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = i + 1; j < 4; j++)
+		{
+			for (k = j + 1; k < 6; k++)
+			{
+				for (l = k + 1; l < 10; l++)
+				{
+					if (i == 2 && j == 3 && k == 5 && l == 9)
+						continue;
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
