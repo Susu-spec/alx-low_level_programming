38598f7d523f74d33fbe2db8d32ce3ad6cbeddba
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-14 12:35:36 -0700

    Add: prints last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ *
+ * @n: int argument
+ *
+ * Return: last digit variable
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
