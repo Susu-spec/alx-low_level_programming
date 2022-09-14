48443a916042ddafc3da7305f55675fb912a60d0
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-14 07:35:04 -0700

    Add: check if lowercase or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_islower - checks if arg is a lowercase char
+ *
+ * @c: int argument
+ *
+ * Return: 1 indicates lowercase, 0 indicates otherwise
+ *
+ */
+
+int _islower(int c)
+{
+	int c;
+
+	if (c == 'a' || c < 'z' || c == 'z')
+		return (1);
+	else
+		return (0);
+}
