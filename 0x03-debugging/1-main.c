72ae21ae6028f12daebef58d9d4a316d31caf62e
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-15 06:54:15 -0700

    Non-working code

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * @while function - @i does not increment
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loopincoming :(\n");
+
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
+
+	printf("Infinte loop avoided!\n");
+
+	return (0);
+}
