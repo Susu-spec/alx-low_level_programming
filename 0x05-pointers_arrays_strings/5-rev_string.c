f4ce3404efc1584cab5accd1724cc3de926e31ae
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-19 07:48:35 -0700

    File: reverses string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+void rev_string(char *s)
+{
+	char temp, rev;
+	int i, n;
+	n = strlen(s);
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		temp = s[i];
+		rev = s[n - i - 1];
+		s[i] = rev;
+		s[n - i - 1] = temp;
+	}
+}
