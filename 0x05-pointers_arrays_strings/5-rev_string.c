24eda8497a91c3071de3dfcdaf320599bdf7f00b
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-19 07:53:42 -0700

    Modify: Betty style and documentation

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,12 +2,17 @@
 #include <string.h>
 #include <stdio.h>
 
+/**
+ * rev_string - reverses string characters
+ * @s: string parameter
+ */
+
 void rev_string(char *s)
 {
 	char temp, rev;
 	int i, n;
-	n = strlen(s);
 
+	n = strlen(s);
 	for (i = 0; i < (n / 2); i++)
 	{
 		temp = s[i];
