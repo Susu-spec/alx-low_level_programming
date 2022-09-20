e7d6706ef5333c6ff5a14a4f74ce24bdf730043d
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-19 19:57:33 -0700

    mend
    Add; rght file

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -1,19 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 
-/**
- * _strlen - returns length of string
- * @s: argument value
- * Return: length of string
- */
-
-int _strlen(char *s);
+int main(void)
 {
-	int length;
+	char *str;
+	int len;
 
-	while (*s != '\0')
-	{
-		length++;
-	}
-
-	return (length);
+	str = "My first strlen!";
+	len = _strlen(str);
+	printf("%d\n", len);
+	return (0);
 }
