f4b0485c56a7da82fc8576398d25e4087519ed2f
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-20 03:33:53 -0700

    File: converts string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _atoi - converts string to integer
+ * @c: string parameter
+ * Return: converted int
+ */
+
+int _atoi(char *c)
+{
+	unsigned int value = 0;
+	int sign = 1;
+
+	while (*c++)
+	{
+		if (*c == '-')
+			sign *= -1;
+		else if ((*c >= '0') && (*c <= '9'))
+		{
+			value = value  * 10 + (int)(*c - '0');
+		}
+	}
+
+	return (value * sign);
+}
