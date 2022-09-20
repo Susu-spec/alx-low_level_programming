ea0afab55b7f9361a775f7f987112b4a59dc0fb1
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-19 20:07:27 -0700

    File: copies strings through parameters

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string from src to dest buffer
+ * @src: pointer to string to be copied
+ * @dest: pointer to buffer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	whie(*(str + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+
+	return (dest);
+}
