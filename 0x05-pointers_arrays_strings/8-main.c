cd07844f9cfd4555d625ee66533455eb99b8dd2f
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-19 19:56:24 -0700

    File: print n elements of array

---
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int main(void)
+{
+	int array[5];
+
+	array[0] = 0;
+	array[1] = 1;
+	array[2] = 2;
+	array[3] = 3;
+	array[4] = 4;
+	array[5] = 5;
+	print_array(array, 5);
+	return (0);
+}
