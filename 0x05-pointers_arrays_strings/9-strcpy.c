a3ff43da1296e643da71457a9096d59c4eb90671
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-19 20:17:41 -0700

    Modify: betty coding style and documentation

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,13 +4,14 @@
  * _strcpy - copies string from src to dest buffer
  * @src: pointer to string to be copied
  * @dest: pointer to buffer
+ * Return: dest pointer to buffer
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while(*(src + i) != '\0')
+	while (*(src + i) != '\0')
 	{
 		*(dest + i) = *(src + i);
 		i++;
