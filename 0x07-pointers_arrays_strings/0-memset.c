2a93b0438653f7f0626bee73187fd283dd638162
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-26 05:58:44 -0700

    File: memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _memset - fills..
+ * @n: number of bytes pointed to by..
+ * @s: char pointer with the constant..
+ * @b: char
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n-- > 0)
+		*s++ = b;
+	return (s);
+}
