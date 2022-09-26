a67a35d637a05fd935f78333fa3f7f1ec290cb2a
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-26 06:24:54 -0700

    File: memcpy function

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h"
+
+/**
+ * memcpy - copies..
+ * @n - number of bytes
+ * @src: from value at src pointer
+ * @dest: to value at dest pointer
+ * Return: char pointer
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest, src, n);
+	return (dest);
+}
