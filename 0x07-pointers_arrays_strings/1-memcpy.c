6e0997d1cc0f75e15f3a6dcf36e50a78b2a19bf9
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-26 06:28:40 -0700

    Add: library

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,9 @@
 #include "main.h"
-#include <string.h"
+#include <string.h>
 
 /**
- * memcpy - copies..
- * @n - number of bytes
+ * _memcpy - copies..
+ * @n: number of bytes
  * @src: from value at src pointer
  * @dest: to value at dest pointer
  * Return: char pointer
