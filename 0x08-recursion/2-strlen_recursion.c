5bf24ff044e5767740eedec19ed8429439ba6743
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-28 10:41:33 -0700

    File: prints string lengthwith recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - print string length recursively
+ * @s: string pointer
+ * Return: total length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\n')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
