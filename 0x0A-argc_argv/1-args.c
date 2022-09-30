c155b16f522b8a95ffae1839133b5de968a04bb7
Susu-spec <suleimansuwaibat@gmail.com>
2022-09-30 10:35:52 -0700

    Create: 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints number of arguments passed
+ * @argc: total number of arguments
+ * @argv: string arguments
+ * Return: 0 indicates success, 1 indicates failure
+ */
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%i\n", argc);
+	return (0);
+}
