0b3967cf4d8b620599788f48bf6dbcc4cf3f504b
Susu-spec <suleimansuwaibat@gmail.com>
2022-10-03 01:48:55 -0700

    Add: 4-add.c to 0x0A-argc_argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply argument values
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 indicates success, 1 incdicates failure
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, add;
+
+	for (i = 1; i < argc; i++)
+	{
+		if ((*argv[i] < '0') || (*argv[i] > '9'))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else if (argc == 1)
+			printf("0\n");
+		else
+		{
+			add += atoi(argv[i]);
+		}
+	}
+	printf("%i\n", add);
+	return (0);
+}
+
+
+
+
