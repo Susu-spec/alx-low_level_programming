b7211b5d72473ff275c5789cc8b9634947824aa9
Susu-spec <suleimansuwaibat@gmail.com>
2022-10-06 11:10:54 -0700

    Modify: control flow

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,8 +13,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *array;
 	unsigned int i;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 	array = malloc(nmemb * size);
-	if (array == NULL || nmemb == 0 || size == 0)
+	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
 		array[i] = 0;
