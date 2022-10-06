0321338e3130faf1f5e0ef06f9ebfd0a1b5ee38f
Susu-spec <suleimansuwaibat@gmail.com>
2022-10-06 10:43:45 -0700

    Add: 2-calloc.c to 0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "msin.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocate memory for array of ints and set to zero
+ * @nmemb: number of elements in array
+ * @size: size of each element
+ * Return: pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *array;
+	int i;
+
+	array = (int *) malloc(nmemb * size);
+	if (array == NULL || nmemb == 0 || size == 0)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		array[i] = 0;
+	return (array);
+}
+
