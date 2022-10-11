3e0359cec7b6ace2b2451b7ca51455be068257ed
Susu-spec <suleimansuwaibat@gmail.com>
2022-10-11 04:45:17 -0700

    Add: 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner
+ * Return: address stored in pointer
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog = malloc(sizeof(dog_t));
+
+	if (dog == NULL)
+		return (NULL);
+	if (name == NULL || owner == NULL || age < 0)
+		return (NULL);
+	dog->name = malloc(sizeof(name));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(owner));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	dog->name = strcpy(dog->name, name);
+	dog->owner = strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
+	free(dog);
+}
