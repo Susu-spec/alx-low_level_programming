4939505afcf650053a3c6e61faaf42e80c3190a5
Susu-spec <suleimansuwaibat@gmail.com>
2022-10-11 02:06:55 -0700

    Add: 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - initialize dog structure
+ * @d: pointer to dog
+ * @name: name array
+ * @age: dog's age
+ * @owner: dog owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+
