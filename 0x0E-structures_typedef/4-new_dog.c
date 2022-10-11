5beb2d38562fdc14fc84d06f4979e128dd5d948e
Susu-spec <suleimansuwaibat@gmail.com>
2022-10-11 05:24:10 -0700

    Modify: Memory allocated

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,13 +20,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (dog == NULL)
 		return (NULL);
-	dog->name = malloc(sizeof(char) * (sizeof(name)));
+	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->owner = malloc(sizeof(char) * (sizeof(owner)));
+	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
