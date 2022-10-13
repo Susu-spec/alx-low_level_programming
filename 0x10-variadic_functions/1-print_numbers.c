744b7d63ecf8406cce221d2ba3da26a10ac091c4
Susu-spec <suleimansuwaibat@gmail.com>
2022-10-13 07:21:33 -0700

    Add: 2-print_strings.c, 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,25 +11,19 @@
 void print_numbers(const char *seperator, const unsigned int n, ...)
 {
 	va_list args;
-	unsigned int i, value, a;
+	unsigned int i, value;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		value = va_arg(args, int);
-		if (i == n - 1)
-		{
-			printf("%i", value);
-		}
-		else
+		printf("%i", value);
+		if (i != n - 1)
 		{
 			printf("%i", value);
 			if (seperator != NULL)
 			{
-				for (a = 0; a < 2; a++)
-				{
-					printf("%c", seperator[a]);
-				}
+				printf("%s", seperator);
 			}
 		}
 	}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print string arguments
+ * @separator: pointer to ', '
+ * @n: number of arguments
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	const char *string;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(args, const char *);
+		printf("%s", string);
+		if (i != n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+
+	va_end(args);
+}
