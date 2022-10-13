23e2417bb8289bb76d50e8cc5d10c2802894b193
Susu-spec <suleimansuwaibat@gmail.com>
2022-10-13 02:33:49 -0700

    Modify: ceck for null arguments

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,6 +12,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t i;
 
 	i = 0;
+	if (size <= 0)
+		return;
+	if (action == NULL)
+		return;
+	if (array == NULL)
+		return;
 	while (i < size && (array))
 	{
 		action((*array));
