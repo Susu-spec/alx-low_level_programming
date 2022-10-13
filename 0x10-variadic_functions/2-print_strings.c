a4710f75187e8a329078269044bf5bcecd2cbd3d
Susu-spec <suleimansuwaibat@gmail.com>
2022-10-13 07:24:19 -0700

    Modify: check for null case

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		string = va_arg(args, const char *);
-		printf("%s", string);
+		if (string == NULL)
+		{
+			print("(nil)");
+		}
+		else
+			printf("%s", string);
 		if (i != n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
