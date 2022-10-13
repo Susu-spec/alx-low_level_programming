49ec70ef6539cdf8159d3ed1f70e9d3c3e4f8af1
Susu-spec <suleimansuwaibat@gmail.com>
2022-10-13 03:26:37 -0700

    Add: 2-int_index.c to 0x0F-function_pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - returns first element index which @cmp does not retur 0
+ * @array: pointer to first element of array
+ * @size: size of array
+ * @cmp: pointer to function
+ * Return: index
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, pos;
+
+	pos = -1, i = 0;
+	if (size <= 0)
+		return (pos);
+	if (cmp == NULL || array == NULL)
+		return (0);
+	while (i < size)
+	{
+		if (cmp((array[i])) != 0)
+		{
+			pos = i;
+			break;
+		}
+		else
+			i++;
+	}
+	return (pos);
+}
