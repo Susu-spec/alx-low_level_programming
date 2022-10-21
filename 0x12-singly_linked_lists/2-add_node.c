ffaa05f08014853fd12b469a9cce3625eb4c9578
Susu-spec <suleimansuwaibat@gmail.com>
2022-10-21 01:47:18 -0700

    Modify: coding style

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,7 +12,7 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *temp = (list_t*) malloc(sizeof(list_t *));
+	list_t *temp = (list_t *) malloc(sizeof(list_t *));
 
 	if (temp == NULL)
 		return (NULL);
