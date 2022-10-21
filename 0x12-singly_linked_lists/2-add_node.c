dbfe3767d2867962ecfab7cf52659f5865faaac5
Susu-spec <suleimansuwaibat@gmail.com>
2022-10-21 01:41:58 -0700

    Add: 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adds node to the beginning of the list
+ * @head: head of the list
+ * @str: string
+ * Return: address of new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp = (list_t*) malloc(sizeof(list_t *));
+
+	if (temp == NULL)
+		return (NULL);
+	temp->str = strdup(str);
+	if (temp->str == NULL)
+		return (NULL);
+	temp->len = strlen(str);
+	temp->next = *head;
+	*head = temp;
+
+	return (temp);
+}
